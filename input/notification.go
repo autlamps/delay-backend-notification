1bfd4ca5e3940dc639948af00164d1c180b2b21e
Hayden Woodhead <hayden.sw@icloud.com>
2017-10-22 13:48:30 +1300

    Added Route, Trip and StopTime info to notification

--- input/notification.go
+++ input/notification.go
@@ -1,10 +1,15 @@
 // Input contains the messages received by the notification service
 package input
 
+import "github.com/autlamps/delay-backend-notification/static"
+
 type Notification struct {
 	TripID     string
 	StopTimeID string
 	Delay      int
 	Lat        float64
 	Lon        float64
+	Route      static.Route
+	Trip       static.Trip
+	StopTimes  []static.StopTime
 }
